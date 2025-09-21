22540d27632dff3df572ab05b955bc6e2f170ee1
thebladehit <boqdanyarmolka@gmail.com>
2025-09-21 14:54:14 +0300

    Change token_auth example to using jwt token

--- token_auth/index.js
+++ token_auth/index.js
@@ -1,101 +1,42 @@
-const uuid = require('uuid');
 const express = require('express');
-const onFinished = require('on-finished');
 const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
-const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "supersecretkey";
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-const SESSION_KEY = 'Authorization';
+const jwtAuthMiddleware = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader?.split(" ")[1].trim();
 
-class Session {
-    #sessions = {}
+    if (!token) {
+        return next();
+    }
 
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
-
-            console.log(this.#sessions);
-        } catch(e) {
-            this.#sessions = {};
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (!err) {
+            req.user = decoded;
         }
-    }
-
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
-
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
-
-    get(key) {
-        return this.#sessions[key];
-    }
-
-    init(res) {
-        const sessionId = uuid.v4();
-        this.set(sessionId);
-
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
-}
-
-const sessions = new Session();
-
-app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
-
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
-    req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
+        next();
     });
+};
 
-    next();
-});
-
-app.get('/', (req, res) => {
-    if (req.session.username) {
+app.get('/', jwtAuthMiddleware, (req, res) => {
+    if (req.user) {
         return res.json({
-            username: req.session.username,
+            username: req.user.username,
             logout: 'http://localhost:3000/logout'
         })
     }
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
     res.redirect('/');
 });
 
@@ -116,17 +57,18 @@ app.post('/api/login', (req, res) => {
     const { login, password } = req.body;
 
     const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
-        }
-        return false
+        return user.login === login && user.password === password;
     });
 
     if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
+        const token = jwt.sign(
+            { username: user.username },
+            JWT_SECRET,
+            {expiresIn: '1d'}
+        );
+        console.log(token);
 
-        res.json({ token: req.sessionId });
+        res.json({ token: token, username: user.username });
     }
 
     res.status(401).send();
